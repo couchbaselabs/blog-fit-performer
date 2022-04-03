8f438dd5236ae5938ddeb4c6127eb8105bdcfc52
praneethreddybokka <42875463+praneethreddybokka@users.noreply.github.com>
2022-04-02 21:36:52 -0700

    Update basicPerformer.java

--- basicPerformer.java
+++ basicPerformer.java
@@ -1,28 +1,31 @@
 if (op.hasInsert()) {
-        //Some code
-        }
+//Transaction Insert logic
+}
 
-        if (op.hasRemove()) {
-        //Some code
-        }
+if (op.hasRemove()) {
+//Transaction Remove logic
+}
 
-        if (op.hasGet()) {
-        //Some code
-        }
+if (op.hasGet()) {
+//Transaction Get logic
+}
+
+//The example used in the blog
 // Reads the transaction object to see if the tests needs a replace operation
-        if (op.hasReplace()) {
-
+if (op.hasReplace()) {
 final CommandReplace request = op.getReplace();
+        
 //Performer gets the new content value of the document
 final JsonObject content = JsonObject.fromJson(request.getContentJson());
-// Retrieve information on the location of the document
+        
+// Retrieve the document on which we need to execute the transaction
 final Collection collection =
         connection.getBucket(request.getDocId().getBucketName())
         .scope(request.getDocId().getScopeName())
         .collection(request.getDocId().getCollectionName());
         String docId = request.getDocId().getDocId() // get docId of the document
+                
 //Finally execute transaction operation
-final TransactionGetResult r = ctx.get(collection, );
-        ctx.replace(r, content);
-        }
-        });
+final TransactionGetResult r = ctx.get(collection, docId);
+ctx.replace(r, content);
+       
